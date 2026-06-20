5a40c4da5de1ea071602ffefab43e3b1eaf2a540
tianf49 <251351131@wastukancana.ac.id>
2026-06-20 13:56:24 +0700

    fixx

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -42,6 +42,6 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerateToken();
 
-        return redirect('/');
+        return redirect()->intended('/'); // Mengarahkan ke halaman utama setelah login
     }
 }
